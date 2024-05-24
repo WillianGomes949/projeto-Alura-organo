1f03770d1dffc3e01b386e306e9d067de8ed6cda
WillianGomes949 <williangomes949@gmail.com>
2024-05-24 09:50:37 -0300

    Inicio do App

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" alt="O banner principal da pagina do organo"/>
+        </header>
+        
+    )
+}
+
+export default Banner
