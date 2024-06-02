bd0b81754249d78dc26a7e0b96a28035329b6f71
WillianGomes949 <williangomes949@gmail.com>
2024-06-02 10:38:53 -0300

    + imagens

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,17 +1,30 @@
-import Colaborador from '../Colaborador'
-import './time.css'
+import Colaborador from '../Colaborador';
+import './time.css';
 
-const Time = ({ time, colaboradores }) => {
-    return (
-
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
-            <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
-            </div>
-        </section>
-
+const Time = ({ time, colaboradores, aoDeletar }) => {
+  return (
+    colaboradores.length > 0 && (
+      <section
+        className="time"
+        style={{
+          backgroundImage: 'url(/imagens/fundo.png)',
+          backgroundColor: time.corPrimaria,
+        }}
+      >
+        <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        <div className="colaboradores">
+          {colaboradores.map((colaborador, indice) => (
+            <Colaborador
+              key={indice}
+              colaborador={colaborador}
+              corDeFundo={time.corSecundaria}
+              aoDeletar={aoDeletar}
+            />
+          ))}
+        </div>
+      </section>
     )
-}
+  );
+};
 
-export default Time
+export default Time;
